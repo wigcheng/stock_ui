602a9ec64fb788e0c77e752e4c82c4ad9ace698e
Wig C <wigcheng@tailyn.com.tw>
2014-08-22 17:03:08 +0800

    Add filter function for stock code.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request
-
 import sys
+import os
+import subprocess
+
 reload(sys)  # Reload does the trick!
 sys.setdefaultencoding('UTF8')
 
@@ -18,7 +20,11 @@ def network(search):
     if search == "search":
         code_input=request.form['codeinput']
         combo_list=request.form['thelist']
-        message=combo_list
+
+        proc = subprocess.Popen(["./mini 20140821.csv %s" % code_input, ""], stdout=subprocess.PIPE, shell=True)
+        (out, err) = proc.communicate()
+        message = out.split("\t")
+
 
     templateData = {
         'message' : message
@@ -26,4 +32,4 @@ def network(search):
     return render_template('index.html',**templateData)
 
 if __name__ == "__main__":
-    app.run(host='172.16.51.28', port=80, debug=True)
+    app.run(host='172.16.51.28', port=8080, debug=True)
