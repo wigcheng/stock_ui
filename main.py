b895591e2f678b9cd964ba717510aebab305bf4f
Wig C <wigcheng@tailyn.com.tw>
2014-08-27 12:42:01 +0800

    Add multi-filter for a fix stock code.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 import sys
 import os
 import subprocess
+import datetime
 
 reload(sys)  # Reload does the trick!
 sys.setdefaultencoding('UTF8')
@@ -20,17 +21,32 @@ def network(search):
     if search == "search":
         code_input=request.form['codeinput']
 
-        date_list = request.form['datelist']
-        if date_list.rfind('-') - date_list.find('-') < 3:
-            date_list = date_list[:date_list.find('-') + 1] + '0' + date_list[date_list.find('-') + 1:]
-        if len(date_list) - date_list.rfind('-') < 3:
-            date_list = date_list[:date_list.rfind('-') + 1] + '0' + date_list[date_list.rfind('-') + 1:]
+        date_list_from = request.form['datelist_from']
+        if date_list_from.rfind('-') - date_list_from.find('-') < 3:
+            date_list_from = date_list_from[:date_list_from.find('-') + 1] + '0' + date_list_from[date_list_from.find('-') + 1:]
+        if len(date_list_from) - date_list_from.rfind('-') < 3:
+            date_list_from = date_list_from[:date_list_from.rfind('-') + 1] + '0' + date_list_from[date_list_from.rfind('-') + 1:]
 
-        date_list= date_list.replace('-', '')
+        #date_list_from= date_list_from.replace('-', ',')
 
-        proc = subprocess.Popen(['data/mini', 'data/' + date_list + '.csv', code_input], stdout=subprocess.PIPE)
-        (out, err) = proc.communicate()
-        message = [date_list] + out.split("\t")
+        date_list_to = request.form['datelist_to']
+        if date_list_to.rfind('-') - date_list_to.find('-') < 3:
+            date_list_to = date_list_to[:date_list_to.find('-') + 1] + '0' + date_list_to[date_list_to.find('-') + 1:]
+        if len(date_list_to) - date_list_to.rfind('-') < 3:
+            date_list_to= date_list_to[:date_list_to.rfind('-') + 1] + '0' + date_list_to[date_list_to.rfind('-') + 1:]
+
+        date_dis = datetime.datetime(*map(int, date_list_to.split('-'))) - datetime.datetime(*map(int, date_list_from.split('-')))
+        print(date_dis.days)
+
+        #date_list_from= date_list_from.replace('-', '')
+
+        message = list()
+
+        for i in range(date_dis.days):
+            date = datetime.date(*map(int, date_list_from.split('-'))) + datetime.timedelta(days=i)
+            proc = subprocess.Popen(['data/mini', 'data/' + date.isoformat().replace('-', '') + '.csv', code_input], stdout=subprocess.PIPE)
+            (out, err) = proc.communicate()
+            message.append([date.isoformat()] + out.split("\t"))
 
 
     templateData = {
