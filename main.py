cab7d6d7155059e502e33c5d3c02aaa184ec4b02
Wig C <wigcheng@tailyn.com.tw>
2014-09-12 14:38:37 +0800

    Add simple research function for code price page.

--- main.py
+++ main.py
@@ -26,25 +26,25 @@ def price():
 @app.route("/<search>" ,methods=['GET' , 'POST'])
 def network(search):
 
-    if search == "search":
-        code_input=request.form['codeinput']
-        the_list=request.form['thelist']
-
-        date_list_from = request.form['datelist_from']
-        if date_list_from.rfind('-') - date_list_from.find('-') < 3:
-            date_list_from = date_list_from[:date_list_from.find('-') + 1] + '0' + date_list_from[date_list_from.find('-') + 1:]
-        if len(date_list_from) - date_list_from.rfind('-') < 3:
-            date_list_from = date_list_from[:date_list_from.rfind('-') + 1] + '0' + date_list_from[date_list_from.rfind('-') + 1:]
+    date_list_from = request.form['datelist_from']
+    if date_list_from.rfind('-') - date_list_from.find('-') < 3:
+        date_list_from = date_list_from[:date_list_from.find('-') + 1] + '0' + date_list_from[date_list_from.find('-') + 1:]
+    if len(date_list_from) - date_list_from.rfind('-') < 3:
+        date_list_from = date_list_from[:date_list_from.rfind('-') + 1] + '0' + date_list_from[date_list_from.rfind('-') + 1:]
 
         #date_list_from= date_list_from.replace('-', ',')
 
-        date_list_to = request.form['datelist_to']
-        if date_list_to.rfind('-') - date_list_to.find('-') < 3:
-            date_list_to = date_list_to[:date_list_to.find('-') + 1] + '0' + date_list_to[date_list_to.find('-') + 1:]
-        if len(date_list_to) - date_list_to.rfind('-') < 3:
-            date_list_to= date_list_to[:date_list_to.rfind('-') + 1] + '0' + date_list_to[date_list_to.rfind('-') + 1:]
+    date_list_to = request.form['datelist_to']
+    if date_list_to.rfind('-') - date_list_to.find('-') < 3:
+        date_list_to = date_list_to[:date_list_to.find('-') + 1] + '0' + date_list_to[date_list_to.find('-') + 1:]
+    if len(date_list_to) - date_list_to.rfind('-') < 3:
+        date_list_to= date_list_to[:date_list_to.rfind('-') + 1] + '0' + date_list_to[date_list_to.rfind('-') + 1:]
 
-        date_dis = datetime.datetime(*map(int, date_list_to.split('-'))) - datetime.datetime(*map(int, date_list_from.split('-')))
+    date_dis = datetime.datetime(*map(int, date_list_to.split('-'))) - datetime.datetime(*map(int, date_list_from.split('-')))
+
+    if search == "search":
+        code_input=request.form['codeinput']
+        the_list=request.form['thelist']
 
         #date_list_from= date_list_from.replace('-', '')
 
@@ -61,6 +61,19 @@ def network(search):
 
             (out, err) = proc.communicate()
             message.append([date.isoformat()] + out.split("\t"))
+    elif search == "price":
+
+        code_input=request.form['codeinput']
+        #date_list_from= date_list_from.replace('-', '')
+        message = list()
+
+#        for i in range(date_dis.days):
+        date = datetime.date(*map(int, date_list_from.split('-')))
+        m = ''.join(map(lambda s: s if len(s) > 1 else '0' + s, map(str, date.timetuple()[:2])))
+        proc = subprocess.Popen(['price/mini','-p', 'price/' + code_input + '/' + m + '_' + code_input + '.csv'], stdout=subprocess.PIPE)
+        (out, err) = proc.communicate()
+#        message = [[l.split('\t') for l in b.split('\n')] for b in out]
+        message = [l.split('\t') for l in out.split('\n')]
 
     templateData = {
         'message' : message
